ba405e7f57c6543bc478e1e625831ae921a85860
karanraghani <karanraghani14@gmail.com>
2018-05-12 07:38:27 -0700

    Added Forms to Create Category and add Pages

---
+++ rango/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+from rango.models import Page, Category
+
+class CategoryForm(forms.ModelForm):
+	name = forms.CharField(max_length=128, help_text="Please enter the Category name")
+	likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+	views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+	slug = forms.CharField(widget=forms.HiddenInput(), required=False)
+
+	class Meta:
+		model = Category
+		fields = ('name',)
+
+class PageForm(forms.ModelForm):
+	title = forms.CharField(max_length=128, help_text="Please enter the name of Website")
+	url = forms.URLField(max_length=200, help_text="Enter the URL")
+	views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+
+	class Meta:
+		model = Page
+		fields = ('title', 'url',)
+
+	def clean(self):
+		cleaned_data = self.cleaned_data
+		url = cleaned_data.get('url') #using .get as it will raise KeyError if value not passed in form
+
+		if url and not url.startswith('http://'):
+			url = 'http://' + url
+			cleaned_data['url'] = url
+
+		return cleaned_data #to commit the changes
