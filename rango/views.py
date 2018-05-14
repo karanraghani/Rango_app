879bec6756965c3726229450b7eb3e074ecb9c08
karanraghani <karanraghani14@gmail.com>
2018-05-14 10:28:45 -0700

    Added cookies to count visits

--- rango/views.py
+++ rango/views.py
@@ -5,6 +5,7 @@ from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 from django.contrib.auth import authenticate, login, logout
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
 
 
 def index(request):
@@ -15,6 +16,9 @@ def index(request):
 	context_dict['categories'] = category_list
 	context_dict['pages'] = page_list
 	
+	visitor_cookie_handler(request)
+	context_dict['visits'] = request.session['visits']
+
 	return render(request, 'rango/index.html', context_dict)
 
 def about(request):
@@ -137,6 +141,29 @@ def login(request):
 def logout(request):
 	logout(request)
 	return HttpResponseRedirect(reverse('index'))
+
+# Helper function 
+def get_server_side_cookie(request, cookie, default_val=None):
+	val = request.session.get(cookie)
+	if not val:
+		val = default_val
+	
+	return val
+
+def visitor_cookie_handler(request):
+	visits = int(get_server_side_cookie(request, 'visits', '1'))
+	last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+
+	last_visit_time = datetime.strptime(last_visit_cookie[:-7],'%Y-%m-%d %H:%M:%S')
+
+	if (datetime.now()-last_visit_time).days > 0:
+		visits = visits + 1
+		request.session['last_visit'] = str(datetime.now())
+	else:
+		request.session['last_visit'] = last_visit_cookie
+
+	request.session['visits'] = visits
+
 				
 
 
